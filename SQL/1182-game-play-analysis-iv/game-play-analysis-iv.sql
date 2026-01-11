49c5dc574d590a2b9bb9e6ce8300080c1faf6016
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-11 05:06:00 -0700

    Time: 4058 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH Act2 AS(
+    SELECT player_id,event_date, LEAD(event_date,1) OVER (partition by player_id order by event_date) AS Lead_date FROM Activity 
+),
+Act3 AS(
+    SELECT player_id,event_date,Lead_date from Act2 WHERE event_date = (
+        SELECT MIN(event_date)
+        FROM Activity a
+        WHERE a.player_id = Act2.player_id
+    )
+)
+SELECT
+    ROUND(
+        SUM(
+            CASE
+                WHEN DATEDIFF(lead_date, event_date) = 1 THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS fraction
+FROM Act3;
