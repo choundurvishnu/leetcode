aedbf1fc42fab058575d419df2d1a76737edd2c1
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-11 04:46:56 -0700

    Time: 588 ms (11.29%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1179-game-play-analysis-i/game-play-analysis-i.sql
+++ SQL/1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,9 +1,16 @@
 # Write your MySQL query statement below
-
-
-#
+With Ranked AS(
+    Select player_id, event_date, Row_number() over(partition by player_id order by event_date) AS rn from Activity
+)
 SELECT
     player_id,
-    min(event_date) AS first_login
-FROM Activity
-group by player_id;
+    event_date AS first_login
+FROM ranked
+WHERE rn = 1;
+
+#
+#SELECT
+#    player_id,
+#    min(event_date) AS first_login
+#FROM Activity
+#group by player_id;
