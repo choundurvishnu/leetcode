7e1209354d97afeab3527164c5ece8773260e03d
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-11 04:19:02 -0700

    Time: 360 ms (46.78%) | Memory: 0B (100.00%) - LeetSync

--- SQL/182-duplicate-emails/duplicate-emails.sql
+++ SQL/182-duplicate-emails/duplicate-emails.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
-SELECT DISTINCT email
-FROM (
-    SELECT email, COUNT(*) OVER (PARTITION BY email) AS cnt
-    FROM Person
-) t
-WHERE cnt > 1;
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+
+# Write your MySQL query statement below
+#SELECT DISTINCT email
+#FROM (
+#    SELECT email, COUNT(*) OVER (PARTITION BY email) AS cnt
+#    FROM Person
+#) t
+#WHERE cnt > 1;
 
