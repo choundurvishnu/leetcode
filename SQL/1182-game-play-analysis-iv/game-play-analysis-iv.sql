b26323110d51148e85db6938c7af7bc100874072
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-11 11:39:19 -0700

    Time: 791 ms (12.11%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ SQL/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 WITH Act2 AS(
     SELECT player_id,event_date, LEAD(event_date,1) OVER (partition by player_id order by event_date) AS Lead_date FROM Activity 
 ),
@@ -20,3 +21,22 @@ SELECT
         2
     ) AS fraction
 FROM Act3;
+*/
+
+
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) /
+        COUNT(DISTINCT f.player_id),
+        2
+    ) AS fraction
+FROM (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_day
+    FROM Activity
+    GROUP BY player_id
+) f
+LEFT JOIN Activity a
+    ON f.player_id = a.player_id
+   AND a.event_date = DATE_ADD(f.first_day, INTERVAL 1 DAY);
