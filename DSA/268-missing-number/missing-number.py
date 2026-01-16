0cd735c2dabca6c8b0060f8df3ca87b7bb863f34
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-15 23:32:41 -0700

    Time: 3 ms (62.23%) | Memory: 20.3 MB (16.12%) - LeetSync

--- DSA/268-missing-number/missing-number.py
+++ DSA/268-missing-number/missing-number.py
@@ -1,8 +1,28 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        total_expect = n*(n+1)/2
-        total_actual = sum(nums)
-        return int(total_expect-total_actual)
+        n=len(nums)
+        xor_sum =0
+        for i in range(n+1):
+            xor_sum ^= i
+        
+        for i in nums:
+            xor_sum ^= i
+        
+        return xor_sum
         
         
+        
+        
+        
+        
+        
+        
+        
+        """      
+                n = len(nums)
+                total_expect = n*(n+1)/2
+                total_actual = sum(nums)
+                return int(total_expect-total_actual)
+                
+        """
+
