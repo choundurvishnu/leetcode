48fa3cd5dfd00ec05d5b691b678068d789fbb156
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-17 00:55:12 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (12.34%) - LeetSync

--- DSA/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ DSA/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -5,12 +5,32 @@ class Solution:
         survivor = 0
         for i in range(2,n+1):
             survivor = (survivor+k)%i
-
-
         return survivor +1
 
 
+        # Method 2 Intuition
+        def josephus(n):
+            if n==1:
+                return 0
+            return (josephus(n-1)+k)%n
+
+        return josephus(n)+1
+
+
         
+# Method 1
+        arr = [i+1 for i in range(n)]
+
+        def helper(arr, start_index):
+            if len(arr)==1:
+                return arr[0]
+            
+            index_to_remove = (start_index+k-1)%len(arr)
+            del arr[index_to_remove]
+            return helper(arr,index_to_remove)
+            
+        
+        return helper(arr,0)
 
 
         
