6deb4278498adb30eaa2b87ed0ecd4f727726201
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-24 14:37:35 -0700

    Time: 45 ms (49.24%) | Memory: 19.4 MB (6.89%) - LeetSync

--- DSA/1013-fibonacci-number/fibonacci-number.py
+++ DSA/1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,28 @@
 class Solution:
     def fib(self, n: int) -> int:
+
+
+
+
+        #----- Memorization--------
+        def fib1(n, ht={0:0,1:1}):
+            if n in ht:
+                return ht[n]
+            else:
+                ht[n] = fib1(n-1,ht)+fib1(n-2,ht)
+                return ht[n]
+        res = fib1(n)
+        return res
+
+
+
+
+
+
+        """
+        #-----Recursive------
         if n <= 1:
             return n
         return self.fib(n-1)+self.fib(n-2)
+        """
         
