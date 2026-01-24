045bccd0ee2d31cf0b6e8f19db9e80d7f68da4bb
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-24 11:42:15 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (29.26%) - LeetSync

---
+++ DSA/78-subsets/subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def helper(nums,i,subset):
+            if i == len(nums):
+                result.append(subset.copy())
+                return
+            
+            helper(nums,i+1,subset)
+            subset.append(nums[i])
+            helper(nums,i+1,subset)
+            subset.pop()
+        helper(nums,0,[])
+
+
+
+
+
+        return result
+        
