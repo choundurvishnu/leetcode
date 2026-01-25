36b99ea9c79cf989c088dead85ca99fd5e71d9cc
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-24 18:19:52 -0700

    Time: 3 ms (70.51%) | Memory: 19.3 MB (36.47%) - LeetSync

--- DSA/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ DSA/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,6 +3,19 @@ class Solution:
 
         """
         """
+        # ---Tabulation
+        n = len(cost)
+        mincost = [0]*(n+1)
+        mincost[0] = 0
+        mincost[1] = 0
+        for i in range(2,n+1):
+            onestep = cost[i-1]+mincost[i-1]
+            twostep = cost[i-2]+mincost[i-2]
+            mincost[i] = min(onestep,twostep)
+        return mincost[n]
+
+
+        """
         #-----Memorization Approach------
         n = len(cost)
         mincost = [-1]*n
@@ -19,6 +32,7 @@ class Solution:
             mincost[index] = min(onestep,twostep)
             return mincost[index]
         return min(helper(0),helper(1))
+        """
         
 
 
