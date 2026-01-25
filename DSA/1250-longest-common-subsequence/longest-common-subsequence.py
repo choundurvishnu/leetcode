41bf90b15e6fb91b5cdc5fd58bc7eb8624ef7216
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-25 11:48:05 -0700

    Time: 350 ms (87.07%) | Memory: 44.3 MB (40.46%) - LeetSync

--- DSA/1250-longest-common-subsequence/longest-common-subsequence.py
+++ DSA/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,6 +1,22 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[n][m]
         
+
+
+
+
+        """
         n = len(text1)
         m = len(text2)
         dp = [[-1]*(m) for _ in range(n)]
@@ -16,6 +32,7 @@ class Solution:
                 dp[index1][index2] =  max(helper(index1,index2+1),helper(index1+1,index2))
             return dp[index1][index2]
         return helper(0,0)
+        """
 
 
         """
