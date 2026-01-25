73e2e3be8b915a1e3f03d9364905abc267cb87c2
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-25 10:36:34 -0700

    Time: 129 ms (55.95%) | Memory: 39.1 MB (20.81%) - LeetSync

--- DSA/494-target-sum/target-sum.py
+++ DSA/494-target-sum/target-sum.py
@@ -1,5 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        summ = sum(nums)
+        dp = [[None]*(2*summ+1) for _ in range(n)]
+
+        def helper(index, sum_nums):
+
+            if index < 0:
+                if sum_nums == target:
+                    return 1
+                else:
+                    return 0
+            if dp[index][sum_nums+summ]!= None:
+                return dp[index][sum_nums+summ]
+            neg = helper(index-1,sum_nums+-1*nums[index])
+            pos = helper(index-1,sum_nums+nums[index]) 
+            dp[index][sum_nums+summ] = neg + pos
+            return dp[index][sum_nums+summ]
+        return helper(n-1,0)
+
+
+
+
+        """
         total = sum(nums)
         if abs(target) > total or (target+total)%2 != 0:
             return 0
@@ -12,4 +35,5 @@ class Solution:
                 dp[s]+=dp[s-num]
         
         return dp[P]
+        """
         
