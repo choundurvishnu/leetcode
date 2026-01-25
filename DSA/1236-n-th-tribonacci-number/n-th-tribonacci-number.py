1a452f1d022e289ba257086c771669a7cc327dc0
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-24 18:28:08 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.66%) - LeetSync

--- DSA/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ DSA/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,5 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        
+        if n==1 or n==2:
+            return 1
+        
+        a,b,c = 0,1,1
+        for _ in range(3,n+1):
+            a,b,c = b,c,a+b+c
+        return c
+
+        """
         dp = [0]*(n+2)
         if n>0:
             dp[1] = 1
@@ -9,4 +21,5 @@ class Solution:
             count+=1
             dp[count] = dp[count-1]+dp[count-2]+dp[count-3]
         return dp[n]
+        """
         
