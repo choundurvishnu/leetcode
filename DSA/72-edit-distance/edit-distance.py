495802ab761fe3debe197cdd75d7fe6712703dee
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-25 12:18:31 -0700

    Time: 23 ms (98.40%) | Memory: 22.5 MB (34.24%) - LeetSync

---
+++ DSA/72-edit-distance/edit-distance.py
@@ -0,0 +1,53 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+
+
+        #---- Memorization----
+        n = len(word1)
+        m = len(word2)
+        dp = [[-1]*m for _ in range(n)]
+
+        def helper(index1,index2):
+            if index1 > n-1 and index2 > m-1:
+                return 0
+            if index1 > n-1:
+                return m-index2
+            if index2 > m-1:
+                return n-index1
+            if dp[index1][index2] !=-1:
+                return dp[index1][index2]
+            
+            if word1[index1] == word2[index2]:
+                dp[index1][index2]= helper(index1+1,index2+1)
+            else:
+                replace = 1 + helper(index1+1,index2+1)
+                delete = 1 + helper(index1+1,index2)
+                insert = 1 + helper(index1,index2+1)
+                dp[index1][index2] = min(replace,delete,insert)
+            return dp[index1][index2]
+        return helper(0,0)
+
+
+        """
+        #---- Recursion --------
+        n = len(word1)
+        m = len(word2)
+
+        def helper(index1,index2):
+            if index1 > n-1 and index2 > m-1:
+                return 0
+            if index1 > n-1:
+                return m-index2
+            if index2 > m-1:
+                return n-index1
+            
+            if word1[index1] == word2[index2]:
+                return helper(index1+1,index2+1)
+            replace = 1 + helper(index1+1,index2+1)
+            delete = 1 + helper(index1+1,index2)
+            insert = 1 + helper(index1,index2+1)
+            return min(replace,delete,insert)
+        return helper(0,0)
+        """
+        
