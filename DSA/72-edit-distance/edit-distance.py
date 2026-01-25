addb2fa7330475167c37f131c5acb814f4783361
Vishnu Choundur <121794500+choundurvishnu@users.noreply.github.com>
2026-01-25 12:34:33 -0700

    Time: 59 ms (54.81%) | Memory: 22.9 MB (24.18%) - LeetSync

--- DSA/72-edit-distance/edit-distance.py
+++ DSA/72-edit-distance/edit-distance.py
@@ -2,7 +2,29 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 
 
+        #----- Tabulation-------
+        n = len(word1)
+        m = len(word2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        for j in range(m+1):
+            dp[0][j] = j
+        
+        for i in range(n+1):
+            dp[i][0] = i
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    replace = 1 + dp[i-1][j-1]
+                    insert = 1 + dp[i][j-1]
+                    delete = 1 + dp[i-1][j]
+                    dp[i][j] = min(replace,insert,delete)
+        return dp[n][m]
 
+
+        """
         #---- Memorization----
         n = len(word1)
         m = len(word2)
@@ -27,6 +49,7 @@ class Solution:
                 dp[index1][index2] = min(replace,delete,insert)
             return dp[index1][index2]
         return helper(0,0)
+        """
 
 
         """
